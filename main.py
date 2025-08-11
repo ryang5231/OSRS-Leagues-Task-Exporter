0a062f48cf664bf209a0e489089edf2cd49cbb7d
Ryan Goh <ryangoh5c@gmail.com>
2025-08-11 15:06:08 +0800

    sheet creating mechanism done

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment
 
 TASK_DIFFICULTY_IDX = 0
 VERBOSE_IDX = 1
 SKILL_REQUIREMENT_IDX = 2
+PERCENT_COMPL_IDX = 3
+rows_to_wrap = ["B", "E"]
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
@@ -33,16 +37,65 @@ points_reference = {
                     "Master": 250
                     }
 
+data = {
+    "Task": [],
+    "Description": [],
+    "Difficulty": [],
+    "Points": [],
+    "Requirement(s)": [],
+    "% Completed": []
+}
+
 # point info: span title
 # text within td
-for r in rows[1:2]:
+for r in rows:
     cols = r.find_all("td")
-    # task_info = r.find_all("td", attrs={"data-sort-value": True})
     if cols:
         task_difficulty = cols[TASK_DIFFICULTY_IDX].find("span", title=True)["title"]
         task_title = cols[TASK_DIFFICULTY_IDX].get_text(" ", strip=True)
         points = points_reference[task_difficulty]
         verbose = cols[VERBOSE_IDX].get_text(" ", strip=False)
 
+        verbose = (
+            verbose
+            .replace("  ", " ")
+            .replace(" .", ".")
+            .replace("\n", "")
+        )
+
+        skill_req = cols[SKILL_REQUIREMENT_IDX].get_text(" ", strip=True)
+        percent_compl = cols[PERCENT_COMPL_IDX].get_text(" ", strip=True)
+
+        data["Task"].append(task_title)
+        data["Description"].append(verbose)
+        data["Difficulty"].append(task_difficulty)
+        data["Points"].append(points)
+        data["Requirement(s)"].append(skill_req)
+        data["% Completed"].append(percent_compl)
+
+df = pd.DataFrame(data)
+file_name = "OSRS_League_Tasks.xlsx"
+df.to_excel(file_name, index=False)
+
+wb = load_workbook(file_name)
+ws = wb.active
+
+for col in ws.columns:
+    max_length = 0
+    col_letter = col[0].column_letter
+    for cell in col:
+        try:
+            if cell.value:
+                max_length = max(max_length, len(str(cell.value)))
+                if col_letter in rows_to_wrap:
+                    cell.alignment = Alignment(wrap_text=True)
+        except:
+            pass
+    adjusted_width = max_length
+    ws.column_dimensions[col_letter].width = adjusted_width
+
+wb.save(file_name)
+
+# print(data)
 # print(soup.prettify())
 
